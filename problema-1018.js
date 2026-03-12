2de5648195dd99bf948be47685ce398f2a760dc3
daniel97 <dani.nicotina@gmail.com>
2026-03-12 17:43:27 -0400

    2do commit problema 1018 resuelto

---
+++ problema-1018.js
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Leer la cantidad de casos de prueba (t)
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+
+            // Procesar cada uno de los t pares
+            for (int i = 0; i < t; i++) {
+                long a = sc.nextLong();
+                long b = sc.nextLong();
+
+                // Determinar la relación
+                if (a > b) {
+                    System.out.println(">");
+                } else if (a < b) {
+                    System.out.println("<");
+                } else {
+                    System.out.println("=");
+                }
+            }
+        }
+        sc.close();
+    }
+}
